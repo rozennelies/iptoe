ef66a3b55236236208471a8f37569382443eb3d4
kevin <kevin.bouret@gmail.com>
2014-08-04 14:51:43 +0200

    ajout inscription

--- src/Wf3/KikaBundle/Controller/TrainingController.php
+++ src/Wf3/KikaBundle/Controller/TrainingController.php
@@ -7,6 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Wf3\KikaBundle\Form\AddTrainingType;
 use Wf3\KikaBundle\Entity\Training;
 use Wf3\KikaBundle\Entity\User;
+use Wf3\KikaBundle\Entity\Subscription;
 
 use \DateTime;
 
@@ -53,18 +54,45 @@ class TrainingController extends Controller
 	public function trainingDetailsAction($id) {
 
 	//récupère le repository de training (pour faire des SELECT)
-    //    $trainingRepository = $this->getDoctrine()->getRepository("Wf3KikaBundle:Training");
+    $trainingRepository = $this->getDoctrine()->getRepository("Wf3KikaBundle:Training");
 
-        // on va déplacer la requete dans le repository 
+    // on va déplacer la requete dans le repository si on veut une rquete plus fine
     //    $training = $trainingRepository->findTrainingDetails($id);
 
+    $user = $this->getUser();
 
-        //shoote ça à la vue
-     //   $params = array (
-     //       "trainingDetails" => $trainingDetails
-     //       );
+    $training = $trainingRepository->find($id);
 
-     //   return $this->render("Wf3KikaBundle:Training:training_details.html.twig",$params);
+
+    // vérifier si l'utilisateur est connecté et déjà inscrit  à cette formation. utilisation du repository subscription
+
+    if($user)    {
+
+    	$subscriptionRepository = $this->getDoctrine()->getRepository("Wf3KikaBundle:Subscription");
+
+    	$subscription = $subscriptionRepository->findBy(
+   			 array('user' => $user,'training' => $training)
+    		 			);
+
+       // $trainings    =  $trainingRepository->findOpenTrainings();
+
+    	print_r($subscription);
+
+
+
+
+    }   
+
+
+
+
+    //shoote ça à la vue
+    $params = array (
+          "trainingDetails" => $training,
+          "user" => $user
+           );
+
+     return $this->render("Wf3KikaBundle:Training:training_details.html.twig",$params);
 
 
 	}
@@ -140,4 +168,56 @@ class TrainingController extends Controller
     {
         return $this->render('Wf3KikaBundle:Default:index.html.twig', array('name' => $name));
     }
+
+
+    public function subscriptionCreateAction($id) {
+
+    	// ici on passe l id de la formation , il faut vérifier que l od est numérique sinon on vire ....
+
+    	$user = $this->getUser();
+
+    	$trainingRepository = $this->getDoctrine()->getRepository("Wf3KikaBundle:Training");
+
+   		$training = $trainingRepository->find($id);
+
+
+   		// on crée l instance subscription
+
+   		$subscription = new Subscription();
+
+
+   		// on passe l objet en entier  correspondant à l'utilisateur
+		$subscription->setUser($user);
+
+		// on passe l objet en entier  correspondant à la formation
+		$subscription->setTraining($training);
+		$subscription->setDateCreated(new DateTime());
+		$subscription->setIsActive(true);
+
+		// pas besoin 
+
+		$em = $this->getDoctrine()->getManager();
+		$em->persist($subscription);
+		$em->flush();	
+
+		// ça plante !!!!!! on n a pas la liste des données
+		//return $this->render('Wf3KikaBundle:Training:open_trainings.html.twig'); 
+
+		return $this->redirect($this->generateUrl('wf3_kika_openTrainings')); 
+  
+
+
+
+
+
+
+    }
+
+    public function subscriptionDeleteAction($id) {
+
+
+    }
+
+
+
 }
